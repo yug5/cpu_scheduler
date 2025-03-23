02081e2c67a6e0fa709e49916efe1e1ceef6af91
ud <yugshrimali82@gmail.com>
2025-03-23 21:08:52 +0530

    u1

--- scheduler/utils.py
+++ scheduler/utils.py
@@ -1,6 +1,9 @@
 def get_process_input():
     n = int(input("Enter number of processes: "))
-    processes = []
+
     for _ in range(n):
         pid = input("Enter Process ID: ")
         arrival = int(input("Enter Arrival Time: "))
+        burst = int(input("Enter Burst Time: "))
+        processes.append((pid, arrival, burst))
+    return processes
