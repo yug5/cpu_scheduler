6c267c4fa80b2312370e94112cfb0bdc663642dd
ud <yugshrimali82@gmail.com>
2025-03-23 21:04:19 +0530

    main

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from scheduler.sjf import sjf
 from scheduler.round_robin import round_robin
 from scheduler.utils import get_process_input
 
-if __name__ == "__main__":
-    print("Choose CPU Scheduling Algorithm:")
-    print("1. First-Come, First-Serve (FCFS)")
-    print("2. Shortest Job First (SJF)")
-    print("3. Round Robin (RR)")
+# if __name__ == "__main__":
+#     print("Choose CPU Scheduling Algorithm:")
+#     print("1. First-Come, First-Serve (FCFS)")
+#     print("2. Shortest Job First (SJF)")
+#     print("3. Round Robin (RR)")
     
-    choice = int(input("Enter choice (1/2/3): "))
-    processes = get_process_input()
+#     choice = int(input("Enter choice (1/2/3): "))
+#     processes = get_process_input()
     
-    if choice == 1:
-        fcfs(processes)
-    elif choice == 2:
-        sjf(processes)
-    elif choice == 3:
-        time_quantum = int(input("Enter Time Quantum for RR: "))
-        round_robin(processes, time_quantum)
-    else:
-        print("Invalid choice.")
+#     if choice == 1:
+#         fcfs(processes)
+#     elif choice == 2:
+#         sjf(processes)
+#     elif choice == 3:
+#         time_quantum = int(input("Enter Time Quantum for RR: "))
+#         round_robin(processes, time_quantum)
+#     else:
+#         print("Invalid choice.")
--- scheduler/fcfs.py
+++ scheduler/fcfs.py
@@ -1,8 +1,8 @@
-def fcfs(processes):
-    processes.sort(key=lambda x: x[1])  
-    time = 0
-    for pid, arrival, burst in processes:
-        if time < arrival:
-            time = arrival
-        print(f"Process {pid} executed from {time} to {time + burst}")
-        time += burst
+# def fcfs(processes):
+#     processes.sort(key=lambda x: x[1])  
+#     time = 0
+#     for pid, arrival, burst in processes:
+#         if time < arrival:
+#             time = arrival
+#         print(f"Process {pid} executed from {time} to {time + burst}")
+#         time += burst
