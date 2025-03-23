4ec1fc8c2c2c4a38f89d978cc9ca618712b265b0
ud <yugshrimali82@gmail.com>
2025-03-23 21:07:26 +0530

    r

--- scheduler/round_robin.py
+++ scheduler/round_robin.py
@@ -5,3 +5,8 @@ def round_robin(processes, time_quantum):
     while queue:
         pid, arrival, burst = queue.pop(0)
         execute_time = min(time_quantum, burst)
+        print(f"Process {pid} executed from {time} to {time + execute_time}")
+        time += execute_time
+        if burst > time_quantum:
+            queue.append((pid, arrival, burst - time_quantum))
+
